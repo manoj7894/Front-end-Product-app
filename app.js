02e9c097be53561d4225f664ef60f09f5cdfc453
Manoj varma <manojvarmapotthuri3003@gmail.com>
2024-09-06 15:11:46 +0530

    1

---
+++ app.js
@@ -0,0 +1,94 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+// const MONGODB_URI =
+//   'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop';
+
+const MONGODB_URI = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
+
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+const csrfProtection = csrf();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+// mongoose
+//   .connect(MONGODB_URI)
+//   .then(result => {
+//     app.listen(3000);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// Updated mongoose connection with deprecation warnings addressed
+// Connect to MongoDB and start the server
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true, // Use new URL parser
+    useUnifiedTopology: true // Use unified topology
+  })
+  .then(result => {
+    console.log('Connected to MongoDB');
+    app.listen(3000, () => {
+      console.log('Server is running on http://localhost:3000');
+    });
+  })
+  .catch(err => {
+    console.log('Failed to connect to MongoDB:', err);
+  });
